6122f6941689b0239ecd0864c5ed91393955377a
Richard Lee <richlee81192@gmail.com>
2019-08-20 19:35:24 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,15 @@
 def run_guessing_game
 random
-
-
+user_prompt
+if input == random_number
+  puts "You guessed the correct number!"
 end
 
 def random
   random_number = rand(6) + 1
+end
+
+def user_prompt
+  puts " Please guess the number between 1 to 6."
+  return input = gets.chomp
 end
